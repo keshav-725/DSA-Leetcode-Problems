393aa789ae3314616782ee8ba7f80d643c61fa2d
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-03-28 23:29:30 +0530

    Time: 11 ms (60.65%), Space: 43.8 MB (75.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,26 +5,15 @@ class Solution {
         int j=0;
         int maxlen = 0;
         while(i<s.length()){
-            char ch;
-            while(i<s.length() && map.getOrDefault(s.charAt(i),0)==0){
-                ch=s.charAt(i);
-                map.put(ch,1);
+            if(i<s.length() && map.getOrDefault(s.charAt(i),0)==0){
+                map.put(s.charAt(i),1);
                 i++;
                 maxlen = Math.max(maxlen,i-j);
-            }
-            if(i<s.length()){
-                ch=s.charAt(i);
-                map.put(ch,map.getOrDefault(ch,0)+1);
-                while(j<i && map.getOrDefault(ch,0)!=1){
-                    char lch = s.charAt(j);
-                    if(map.getOrDefault(lch,0)>1){
-                        map.put(lch,map.getOrDefault(lch,0)-1);
-                    }else if(map.getOrDefault(lch,0)==1){
-                        map.remove(lch);
-                    }
+            }else{
+                if(j<i){
+                    map.put(s.charAt(j),map.get(s.charAt(j))-1);
                     j++;
                 }
-                i++;
             }
         }
         return maxlen;
