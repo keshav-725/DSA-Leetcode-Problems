01c15120c480d6a4c300cfcc16fa2fe50768538a
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-04-04 09:02:03 +0530

    Time: 15 ms (58.98%), Space: 55.6 MB (19.27%) - LeetHub

--- 554-brick-wall/554-brick-wall.java
+++ 554-brick-wall/554-brick-wall.java
@@ -1,17 +1,14 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int maxsum = 0,maxkey=0;
+        int maxsum = 0;
         for(int i=0;i<wall.size();i++){
             int sum = 0;
             for(int j=0;j<wall.get(i).size()-1;j++){
                 int num = wall.get(i).get(j);
                 sum += num;
                 map.put(sum,map.getOrDefault(sum,0)+1);
-               if(maxsum<map.get(sum)){
-                   maxsum = map.get(sum);
-                   maxkey = sum;
-               }
+               maxsum = Math.max(maxsum,map.get(sum));
             }
         }
         int crossbrick = wall.size() - maxsum;
