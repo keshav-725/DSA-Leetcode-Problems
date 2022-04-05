9237f56773c21407c6e94404de7821e727b2cb0c
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-04-05 17:24:55 +0530

    Time: 278 ms (29.54%), Space: 52.3 MB (48.69%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<Pair> list = new ArrayList<>();
+        
+        for(int i=0;i<buildings.length;i++){
+            int start = buildings[i][0];
+            int end = buildings[i][1];
+            int ht = buildings[i][2];
+            
+            list.add(new Pair(start,-ht));
+            list.add(new Pair(end,ht));
+        }
+            Collections.sort(list);
+            
+            List<List<Integer>> ans = new ArrayList<>();
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            
+            int cht = 0;
+            pq.add(0);
+            for(int i=0;i<list.size();i++){
+                int x = list.get(i).x;
+                int ht = list.get(i).height;
+                
+                if(ht<0){
+                    pq.add(-ht);
+                }else if(ht>0){
+                    pq.remove(ht);
+                }
+                
+                if(cht != pq.peek()){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(x);
+                    temp.add(pq.peek());
+                    
+                    ans.add(temp);
+                    
+                    cht = pq.peek();
+                }
+            }
+            return ans;
+    }
+    public class Pair implements Comparable<Pair>{
+        int x;
+        int height;
+        
+        Pair(int x,int height){
+            this.x = x;
+            this.height = height;
+        }
+        
+        public int compareTo(Pair o){
+            if(this.x != o.x){
+                return this.x-o.x;
+            }else{
+                return this.height - o.height;
+            }
+        }
+    }
+}
