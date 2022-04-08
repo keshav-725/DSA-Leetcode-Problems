6e121413129ac2b04f052f6480fc308ced241e1b
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-04-09 00:43:38 +0530

    Time: 13 ms (66.81%), Space: 47 MB (79.02%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,30 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        if(p.length()>s.length()){
-            return list;
-        }
         int []map1 = new int[26];
+        List<Integer> list = new ArrayList<>();
+        if(p.length()>s.length()) return list;
         for(int i=0;i<p.length();i++){
             char ch = p.charAt(i);
             map1[ch-'a']++;
         }
+        int count = 0;
+        for(int i=0;i<26;i++){
+            count += map1[i];
+        }
         int []map2 = new int[26];
-        int j=0;
         for(int i=0;i<p.length();i++){
             char ch = s.charAt(i);
             map2[ch-'a']++;
         }
+        int j=0;
+        int i = p.length();
         if(areAnagram(map1,map2)){
             list.add(j);
         }
-        for(int i=p.length();i<s.length();i++){
+        for(;i<s.length();i++){
             char ch = s.charAt(i);
             char prevch = s.charAt(j);
             map2[ch-'a']++;
-            if(map2[prevch-'a']>0){
-                map2[prevch-'a']--;
-            }
+            map2[prevch-'a']--;
             j++;
             if(areAnagram(map1,map2)){
                 list.add(j);
@@ -32,11 +33,9 @@ class Solution {
         }
         return list;
     }
-    public boolean areAnagram(int[] map1,int[]map2){
+    public boolean areAnagram(int []map1,int []map2){
         for(int i=0;i<26;i++){
-            if(map1[i]!=map2[i]){
-                return false;
-            }
+            if(map1[i]!=map2[i]) return false;
         }
         return true;
     }
