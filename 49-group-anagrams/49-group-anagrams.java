066036cd0c3e4b255b72a3f13d1d2f8f43d69cc7
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-04-09 02:14:54 +0530

    Time: 75 ms (9.48%), Space: 49 MB (82.37%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
         HashMap<HashMap<Character,Integer>,ArrayList<String>> map = new HashMap<>();
-        
         for(int i=0;i<strs.length;i++){
-            HashMap<Character,Integer> charcountmap = new HashMap<>();
-            for(int j=0;j<strs[i].length();j++){
-                char ch = strs[i].charAt(j);
-                charcountmap.put(ch,charcountmap.getOrDefault(ch,0)+1);
+            String str = strs[i];
+            HashMap<Character,Integer> inmap = new HashMap<>();
+            for(int j=0;j<str.length();j++){
+                char ch = str.charAt(j);
+                inmap.put(ch,inmap.getOrDefault(ch,0)+1);
             }
-            if(map.containsKey(charcountmap)){
-                map.get(charcountmap).add(strs[i]);
-            }else{
-                ArrayList<String> list = new ArrayList<>();
-                list.add(strs[i]);
-                map.put(charcountmap,list);
+            if(!map.containsKey(inmap)){
+                map.put(inmap,new ArrayList<>());
             }
+            map.get(inmap).add(str);
         }
         for(HashMap<Character,Integer> tempmap : map.keySet()){
-            res.add(map.get(tempmap));
+            ans.add(map.get(tempmap));
         }
-        return res;
+        return ans;
     }
 }
