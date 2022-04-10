5c8046e7f1089d34a66818f4e6fdf73707c3a998
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-04-10 17:41:45 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (98.26%) - LeetHub

--- 970-powerful-integers/970-powerful-integers.java
+++ 970-powerful-integers/970-powerful-integers.java
@@ -1,28 +1,20 @@
 class Solution {
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        ArrayList<Integer> list = new ArrayList<>();
         HashSet<Integer> set = new HashSet<>();
+        List<Integer> ans = new ArrayList<>();
         for(int i=0;Math.pow(x,i)<=bound;i++){
-            if(y==1){
-                int sum = (int)Math.pow(x,i)+1;
-                if(sum<=bound && !set.contains(sum)){
-                    list.add(sum);
-                    set.add(sum);
-                }
-            }
-            else{
-                for(int j=0;Math.pow(y,j)<=bound;j++){
-                    int sum = (int)Math.pow(x,i)+(int)Math.pow(y,j);
-                    if(sum<=bound && !set.contains(sum)){
-                        list.add(sum);
+            for(int j=0;Math.pow(y,j)<=bound;j++){
+                int sum = (int)Math.pow(x,i)+(int)Math.pow(y,j);
+                if(sum<=bound){
+                    if(!set.contains(sum)){
                         set.add(sum);
+                        ans.add(sum);
                     }
                 }
+                if(y==1) break;
             }
-            if(x==1){
-                break;
-            }
+            if(x==1) break;
         }
-        return list;
+        return ans;
     }
 }
