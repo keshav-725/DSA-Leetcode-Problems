03b7f37861cfba61fceb279cf03e3faca6e6f205
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-05-18 19:34:46 +0530

    Time: 123 ms (77.65%), Space: 40 MB (97.01%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,38 +1,30 @@
 class Solution {
-    boolean isExist;
     public boolean exist(char[][] board, String word) {
-        isExist = false;
         char ch = word.charAt(0);
         boolean [][]vis = new boolean[board.length][board[0].length];
-        findCharPosition(board,vis,ch,0,0,word);
-        return isExist;
-    }
-    public void findCharPosition(char[][] board,boolean [][]visited,char ch,int row,int col,String word){
-        if(row<0 || col<0 || row>=board.length || col>=board[0].length || visited[row][col]==true) return;
-        if(ch==board[row][col]){
-            //System.out.println(ch);
-            boolean [][]vis = new boolean[board.length][board[0].length];
-            checkExist(board,vis,row,col,""+ch,word,0);
-            //System.out.println("___________________________________________");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(ch==board[i][j]){
+                    boolean isExist = dfsBoard(board,vis,i,j,word,0);
+                    if(isExist) return true;
+                }
+            }
         }
-        visited[row][col]=true;
-        findCharPosition(board,visited,ch,row+1,col,word);
-        findCharPosition(board,visited,ch,row-1,col,word);
-        findCharPosition(board,visited,ch,row,col+1,word);
-        findCharPosition(board,visited,ch,row,col-1,word);
+        return false;
     }
-    public void checkExist(char[][] board,boolean [][]vis,int i,int j,String words,String word,int count){
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || vis[i][j]==true || count>=word.length() || word.charAt(count)!=board[i][j]) return;
-        //System.out.println(words);
-        //String tword = words+board[i][j];
-        if(word.equals(words)){
-            isExist = true;
-        }
-        vis[i][j]=true;
-        if(i+1<board.length) checkExist(board,vis,i+1,j,words+board[i+1][j],word,count+1);
-        if(i-1>=0) checkExist(board,vis,i-1,j,words+board[i-1][j],word,count+1);
-        if(j+1<board[0].length) checkExist(board,vis,i,j+1,words+board[i][j+1],word,count+1);
-        if(j-1>=0) checkExist(board,vis,i,j-1,words+board[i][j-1],word,count+1);
-        vis[i][j]=false;
-    }
+    public boolean dfsBoard(char[][] board, boolean[][] visitedBoard, int row, int col, String word, int wordInd) {        			
+		if( (row < 0 || row >= board.length)   ||   (col < 0 || col >= board[0].length)  ||   board[row][col] != word.charAt(wordInd) ){
+			return false;                                                                             
+		}
+		if(visitedBoard[row][col] ){ return false; }                                                  
+		if(wordInd == word.length() - 1){ return true; }
+		visitedBoard[row][col] = true;                                                                
+			
+		if( dfsBoard(board, visitedBoard, row, col-1, word, wordInd + 1) ){ return true; }            
+		if( dfsBoard(board, visitedBoard, row, col+1, word, wordInd + 1) ){ return true; }            
+		if( dfsBoard(board, visitedBoard, row-1, col, word, wordInd + 1) ){ return true; }            
+		if( dfsBoard(board, visitedBoard, row+1, col, word, wordInd + 1) ){ return true; }            
+		visitedBoard[row][col] = false;
+		return false;
+	}
 }
