7d4abfe8434469bc9a791491a30f72ff1bc91073
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-05-25 08:57:17 +0530

    Time: 321 ms (23.52%), Space: 297.3 MB (59.61%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,66 +1,51 @@
 class Solution {
+    public int time = 0;
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        List<Set<Integer>> adj = new ArrayList<>();
-        int[] parent = new int[n];
-        for(int i=0;i<n;i++){
-            adj.add(new HashSet<>()); // We use hashset so that removal of edge is quick
-            parent[i] = -1;
+        List<List<Integer>> ans = new ArrayList<>();
+        HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
+        for(int i=0;i<connections.size();i++){
+            int start = connections.get(i).get(0);
+            int end = connections.get(i).get(1);
+            
+            graph.putIfAbsent(start,new ArrayList<>());
+            graph.putIfAbsent(end,new ArrayList<>());
+            
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
-        for(List<Integer> edge: connections) {
-            adj.get(edge.get(0)).add(edge.get(1));
-            adj.get(edge.get(1)).add(edge.get(0));
-        }
-        
-        Stack<Integer> stack = new Stack<>();
-        boolean[] visited = new boolean[n];
-        for(int i=0;i<n;i++) {
-            if(!visited[i]){
-                getOrder(adj, stack, parent, visited, i); // Fill stack for ordering
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            if(parent[i]!=-1){
-                adj.get(parent[i]).remove(i); // This is similar to the case where we have to build the transpose graph as per Kosaraju's Algo
-            }
-        }
-        Arrays.fill(visited, false);
-        
-        List<List<Integer>> criticals = new ArrayList<>();
-        while(!stack.isEmpty()) {
-            int v = stack.pop();
-            if(!visited[v]){
-                if(parent[v]!=-1){
-                    criticals.add(Arrays.asList(parent[v], v)); // Whenever we pop from stack and the component is unvisited it means a new SCC 
+        // int n=graph.size();
+        int []parent = new int[n];
+        int []disc = new int[n];
+        int []low = new int[n];
+        boolean []vis = new boolean[n];
+        boolean []art = new boolean[n];
+        parent[0]=-1;
+        time=0;
+        articulationGraph(ans,graph,0,parent,disc,low,vis,art);
+        return ans;
+    }
+    public void articulationGraph(List<List<Integer>> ans,HashMap<Integer,ArrayList<Integer>> graph,int src,int []parent,int []disc,int []low,boolean []vis,boolean []art){
+        disc[src]=low[src]=time;
+        time++;
+        vis[src]=true;
+        int count=0;
+        for(int nbr:graph.get(src)){
+            if(vis[nbr]==true && parent[src]!=nbr){
+                low[src] = Math.min(low[src],disc[nbr]);
+            }else if(vis[nbr]==false){
+                parent[nbr]=src;
+                articulationGraph(ans,graph,nbr,parent,disc,low,vis,art);
+                low[src]= Math.min(low[src],low[nbr]);
+                count++;
+                if(disc[src]<low[nbr]){
+                    art[src]=true;
+                    List<Integer> tans = new ArrayList<>();
+                    tans.add(src);
+                    tans.add(nbr);
+                    ans.add(tans);
                 }
-                dfs(adj, visited, v);
             }
         }
-        
-        return criticals;
-    }
-    
-    private void getOrder(List<Set<Integer>> adj, Stack<Integer> stack, int[] parent, boolean[] visited, int s) {
-        visited[s] = true;
-        
-        for(int n: adj.get(s)) {
-            if(!visited[n]){
-                parent[n] = s;
-                getOrder(adj, stack, parent, visited, n);
-            }
-        }
-        
-        stack.push(s);
-    }
-    
-    private void dfs(List<Set<Integer>> adj, boolean[] visited, int s) {
-        visited[s] = true;
-        
-        for(int n: adj.get(s)) {
-            if(!visited[n]){
-                dfs(adj, visited, n);
-            }
-        }
-        
+                      
     }
 }
