b1796209be9c5ac889e4a639547c2102a8bb4320
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-05-30 08:10:37 +0530

    Time: 835 ms (16.29%), Space: 120.4 MB (27.81%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,20 +1,54 @@
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
-        int n = points.length, curr = 0, count = 0, ans = 0;
-        int[] minDist = new int[n];
-        Arrays.fill(minDist, 2_000_000_000);
+    public int find(int []parent,int x){
+        if(parent[x]==x) return x;
         
-        while(++count<n){
-            minDist[curr] = Integer.MAX_VALUE;
-            int next = 0;
-            for(int i=0;i<n;i++){
-                if(minDist[i] != Integer.MAX_VALUE){
-                    minDist[i] = Math.min(minDist[i], Math.abs(points[curr][0] - points[i][0])+Math.abs(points[curr][1] - points[i][1]));
-                    if(minDist[i] < minDist[next]) next = i;
-                }
-            }
-            ans += minDist[(curr = next)];
+        int res = find(parent,parent[x]);
+        parent[x]=res;
+        return res;
+    }
+    public void union(int lx,int ly,int []parent,int []rank){
+        
+        if(rank[lx]<rank[ly]){
+            parent[lx]=ly;
+        }else if(rank[ly]<rank[lx]){
+            parent[ly]=lx;
+        }else{
+            parent[lx]=ly;
+            rank[ly]++;
         }
-        return ans;
+    }
+    public int minCostConnectPoints(int[][] points) {
+        int size = points.length * (points.length-1)/2;
+        int [][]coord = new int[size][3];
+        int idx=0;
+        for(int i=0;i<points.length;i++){
+            for(int j=i+1;j<points.length;j++){
+                int dist = Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
+                int []cord = {i,j,dist};
+                coord[idx]=cord;
+                idx++;
+            }
+        }
+        Arrays.sort(coord,(val1,val2)->Integer.compare(val1[2],val2[2]));
+        int cost=0;
+        int []parent = new int[points.length];
+        int []rank = new int[points.length];
+        for(int i=0;i<points.length;i++){
+            parent[i]=i;
+            rank[i]=1;
+        }
+        for(int i=0;i<size;i++){
+            int u=coord[i][0];
+            int v =coord[i][1];
+            int w = coord[i][2];
+            
+            int lu = find(parent,u);
+            int lv = find(parent,v);
+            
+            if(lu==lv) continue;
+            union(lu,lv,parent,rank);
+            cost+=w;
+        }
+        return cost;
     }
 }
