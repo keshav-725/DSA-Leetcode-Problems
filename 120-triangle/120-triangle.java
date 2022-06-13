b1e594588235767ffd0fb11a68a1188e29d60457
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-06-14 00:16:59 +0530

    Time: 4 ms (61.51%), Space: 41.8 MB (95.49%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,30 +1,32 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> temp1 = new ArrayList<>();
+        List<Integer> prev = new ArrayList<>();
+        List<Integer> next = new ArrayList<>();
         for(int j=0;j<triangle.get(0).size();j++){
-            temp1.add(triangle.get(0).get(j));
+            prev.add(triangle.get(0).get(j));
         }
-        res.add(temp1);
+        if(triangle.size()==1){
+            next = prev;
+        }
         for(int i=1;i<triangle.size();i++){
             List<Integer> temp = triangle.get(i);
-            List<Integer> plist = res.get(i-1);
-            List<Integer> tlist = new ArrayList<>();
+            next = new ArrayList<>();
             for(int j=0;j<temp.size();j++){
                 if(j==0){
-                    tlist.add(plist.get(0)+temp.get(0));
+                    next.add(prev.get(0)+temp.get(0));
                 }else if(j==temp.size()-1){
-                    tlist.add(plist.get(plist.size()-1)+temp.get(temp.size()-1));
+                    next.add(prev.get(prev.size()-1)+temp.get(temp.size()-1));
                 }else{
-                    int min = Math.min(plist.get(j),plist.get(j-1));
-                    tlist.add(min+temp.get(j));
+                    int min = Math.min(prev.get(j),prev.get(j-1));
+                    next.add(min+temp.get(j));
                 }
             }
-            res.add(tlist);
+            prev = next;
         }
         int ans = Integer.MAX_VALUE;
-        for(int i=0;i<res.get(res.size()-1).size();i++){
-            ans = Math.min(ans,res.get(res.size()-1).get(i));
+        for(int i=0;i<prev.size();i++){
+            ans = Math.min(ans,prev.get(i));
         }
         return ans;
     }
