ff72d5f73e66ab8baface83bd435422a8e5a1613
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-07-16 15:01:21 +0530

    Time: 7 ms (33.71%), Space: 58.4 MB (7.60%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,28 +1,24 @@
 class Solution {
     int [][]dist = {{-1,0},{0,-1},{1,0},{0,1}};
     public int numIslands(char[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        boolean [][]visited = new boolean[row][col];
         int count=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]=='1' && visited[i][j]==false){
-                    checkIsland(i,j,grid,visited);
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    checkIsland(i,j,grid);
                     count++;
                 }
             }
         }
         return count;
     }
-    public void checkIsland(int row,int col,char [][]grid,boolean [][]visited){
-        if(grid[row][col]=='0') return;
-        visited[row][col]=true;
+    public void checkIsland(int row,int col,char [][]grid){
+        grid[row][col]='0';
         for(int i=0;i<4;i++){
             int nrow = row+dist[i][0];
             int ncol = col+dist[i][1];
-            if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && visited[nrow][ncol]==false){
-                checkIsland(nrow,ncol,grid,visited);
+            if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && grid[nrow][ncol]=='1'){
+                checkIsland(nrow,ncol,grid);
             }
         }
     }
