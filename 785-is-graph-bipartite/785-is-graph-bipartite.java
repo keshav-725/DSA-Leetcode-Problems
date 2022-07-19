4ff70637eccd149ddabe199c0c42a76879a469a8
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-07-19 20:02:39 +0530

    Time: 2 ms (58.23%), Space: 43.1 MB (88.97%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,37 +1,37 @@
 class Solution {
+    public class Pair{
+        int val;
+        char color;
+        Pair(int val,char color){
+            this.val = val;
+            this.color = color;
+        }
+    }
     public boolean isBipartite(int[][] graph) {
-        int color[] = new int[graph.length];
-        Arrays.fill(color, -1);
-        
-        for(int i=0;i<graph.length;i++) {
-            if(color[i] == -1) {
-                if(!BFS(i,color,graph)) {
-                    return false;
-                }
+        LinkedList<Pair> queue = new LinkedList<>();
+        HashMap<Integer,Character> map = new HashMap<>();
+        for(int i=0;i<graph.length-1;i++){
+            if(!map.containsKey(i)){
+                queue.add(new Pair(i,'r'));
+                map.put(i,'r');
+                boolean check = addToQueue(i,graph,queue,map);
+                if(check==false) return false;
             }
         }
         return true;
     }
-    
-    public boolean BFS(int curNode, int color[],int[][] graph) {
-        Queue<Integer> que = new LinkedList<>();
-        que.add(curNode);
-        color[curNode] = 0;
-        int curColor = 0;
-        
-        while(!que.isEmpty()) {
-            int node = que.remove();
-            curColor = color[node] == 0 ? 1 : 0;
-            int adjNodes[] = graph[node];
-            for(int adjNode : adjNodes) {
-                if(color[adjNode] == -1) {
-                    color[adjNode] = curColor;
-                    que.add(adjNode);
-                } else {
-                    if(color[adjNode] != curColor) {
-                        return false;
-                    }
+    public boolean addToQueue(int j,int [][]graph,LinkedList<Pair> queue,HashMap<Integer,Character> map ){
+        while(queue.size()>0){
+            Pair rem = queue.removeFirst();
+            for(int i=0;i<graph[rem.val].length;i++){
+                char ncolor = rem.color=='r'?'g':'r';
+                int num = graph[rem.val][i];
+                if(map.containsKey(num)){
+                    if(map.get(num)!=ncolor) return false;
+                    else continue;
                 }
+                map.put(num,ncolor);
+                queue.addLast(new Pair(num,ncolor));
             }
         }
         return true;
