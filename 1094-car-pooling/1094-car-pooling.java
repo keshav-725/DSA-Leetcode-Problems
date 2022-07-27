44ab08fefa28c71bedb73cbdef20455af77175ab
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-07-27 23:00:21 +0530

    Time: 10 ms (29.76%), Space: 44.6 MB (62.20%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int [][]start = Arrays.copyOf(trips,trips.length);
-        int [][]end = Arrays.copyOf(trips,trips.length);
-        
-        Arrays.sort(start,(a,b)->Integer.compare(a[1],b[1]));
-        Arrays.sort(end,(a,b)->Integer.compare(a[2],b[2]));
-        int istart = 0,iend =0;
-        int max=0,count=0;
-        
-        while(istart<trips.length){
-            if(start[istart][1]<end[iend][2]){
-                count+=start[istart][0];
-                istart++;
-            }else if(start[istart][1]>end[iend][2]){
-                count-=end[iend][0];
-                iend++;
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i=0;i<trips.length;i++){
+            int stime = trips[i][1];
+            int etime = trips[i][2];
+            int passenger = trips[i][0];
+            if(map.containsKey(stime)){
+                map.put(stime,map.get(stime)+passenger);
             }else{
-                count += start[istart][0];
-                count -= end[iend][0];
-                istart++;
-                iend++;
+                map.put(stime,passenger);
             }
+            if(map.containsKey(etime)){
+                map.put(etime,map.get(etime)-passenger);
+            }else{
+                map.put(etime,-passenger);
+            }
+        }
+        int count = 0,max = 0;
+        for(int key : map.keySet()){
+            count += map.get(key);
             max = Math.max(count,max);
         }
         return max<=capacity;
