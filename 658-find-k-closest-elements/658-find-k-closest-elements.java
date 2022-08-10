1c097420de16a1dec84ea714788d0fd343959250
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-08-10 23:33:14 +0530

    Time: 16 ms (37.44%), Space: 62.6 MB (51.46%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,40 +1,42 @@
 class Solution {
+    public int lowerbound(int []arr,int target){
+        int left = 0;
+        int right = arr.length-1;
+        int ans=arr.length;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(arr[mid]>=target){
+                ans = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return ans;
+    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int ridx = getIndex(arr,x);
-        int lidx = ridx-1;
+        int rindex = lowerbound(arr,x);
+        int lindex = rindex-1;
         List<Integer> list = new ArrayList<>();
         while(k>0){
-            int lval = Integer.MAX_VALUE;
-            int rval = Integer.MAX_VALUE;
-            if(lidx>=0){
-                lval = Math.abs(arr[lidx]-x);
-            }
-            if(ridx<arr.length){
-                rval = Math.abs(arr[ridx]-x);
-            }
-            if(rval>=lval){
-                list.add(arr[lidx]);
-                lidx--;
-            }else{
-                list.add(arr[ridx]);
-                ridx++;
+            if(lindex>=0 && rindex<arr.length){
+                if((x-arr[lindex])<=(arr[rindex]-x)){
+                    list.add(arr[lindex]);
+                    lindex--;
+                }else{
+                    list.add(arr[rindex]);
+                    rindex++;
+                }
+            }else if(lindex>=0){
+                list.add(arr[lindex]);
+                lindex--;
+            }else if(rindex<arr.length){
+                list.add(arr[rindex]);
+                rindex++;
             }
             k--;
         }
         Collections.sort(list);
         return list;
-    }
-    public int getIndex(int []arr,int x){
-        int left=0,right=arr.length-1;
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(arr[mid]==x) return mid;
-            else if(arr[mid]<x){
-                left = mid+1;
-            }else{
-                right = mid-1;
-            }
-        }
-        return left;
     }
 }
