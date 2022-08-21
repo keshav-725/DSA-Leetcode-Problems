bb9ef33fa794fcdd69a96e653273feef3fa20985
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-08-21 23:38:51 +0530

    Time: 4 ms (25.12%), Space: 45.1 MB (25.98%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        getPermutation(nums,ans,list,set,0);
+        return ans;
+    }
+    public void getPermutation(int []nums,List<List<Integer>> ans,List<Integer> list,HashSet<Integer> set,int idx){
+        if(idx==nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                list.add(nums[i]);
+                set.add(nums[i]);
+                getPermutation(nums,ans,list,set,idx+1);
+                list.remove(list.size()-1);
+                set.remove(nums[i]);
+            }
+            
+        }
+    }
+}
