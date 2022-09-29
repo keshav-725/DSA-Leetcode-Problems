971a6e04968aec43951ddde3b801c34f0c00a1de
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-09-29 09:02:46 +0530

    Time: 8 ms (93.82%), Space: 41.9 MB (97.53%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.java
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -1,41 +1,41 @@
 class Solution {
-    public class Pair implements Comparable<Pair>{
-        int x;
-        int y;
-        int dist;
-        Pair(int x,int y,int dist){
+    int [][]dir = {{-1,0},{0,-1},{1,0},{0,1}};
+    class Pair implements Comparable<Pair>{
+        int x,y,time;
+        Pair(int x,int y,int time){
             this.x = x;
-            this.y = y;
-            this.dist = dist;
+            this.y=y;
+            this.time = time;
         }
-        public int compareTo(Pair o){
-            return this.dist-o.dist;
+        public int compareTo(Pair other){
+            return this.time - other.time;
         }
     }
-    int [][]dir = {{-1,0},{0,-1},{1,0},{0,1}};
     public int swimInWater(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        boolean [][]visited = new boolean[row][col];
         PriorityQueue<Pair> queue = new PriorityQueue<>();
-        boolean [][]visited = new boolean[grid.length][grid[0].length];
         queue.add(new Pair(0,0,grid[0][0]));
-        int max = 0;
         visited[0][0]=true;
+        int max = -1;
         while(queue.size()>0){
-            Pair rem = queue.remove();
-            max = Math.max(max,rem.dist);
-            // System.out.println(rem.dist);
-            if(rem.x == grid.length-1 && rem.y == grid[0].length-1){
-                break;
-            }
-            for(int i=0;i<4;i++){
-                int nrow = rem.x + dir[i][0];
-                int ncol = rem.y + dir[i][1];
+            Pair rem = queue.poll();
+            // System.out.println(rem.x +" " + rem.y+" " +rem.time);
+            max = Math.max(max,rem.time);
+            if(rem.x==row-1 && rem.y==col-1) return max;
+            
+            for(int j=0;j<dir.length;j++){
+                int nrow = rem.x + dir[j][0];
+                int ncol = rem.y + dir[j][1];
                 
-                if(nrow>=0 && ncol >=0 && nrow < grid.length && ncol<grid[0].length && visited[nrow][ncol]==false){
+                if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && visited[nrow][ncol]==false){
                     visited[nrow][ncol]=true;
                     queue.add(new Pair(nrow,ncol,grid[nrow][ncol]));
                 }
             }
         }
-        return max;
+        return -1;
     }
+    
 }
