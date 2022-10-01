a9859fafbdad5514bc746117a73ec169bf9b4cca
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-01 10:58:50 +0530

    Time: 1 ms (91.65%), Space: 44 MB (48.40%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -2,42 +2,39 @@ class Solution {
     public int find(int []parent,int x){
         if(parent[x]==x) return x;
         
-        int temp = find(parent,parent[x]);
-        parent[x]=temp;
-        return temp;
+        parent[x]=find(parent,parent[x]);
+        return parent[x];
+    }
+    public void union(int []parent,int []rank,int lx,int ly){
+        if(rank[lx]<rank[ly]){
+            parent[lx] = ly;
+        }else if(rank[ly]<rank[lx]){
+            parent[ly] = lx;
+        }else{
+            parent[lx] = ly;
+            rank[ly]++;
+        }
     }
     public int[] findRedundantConnection(int[][] edges) {
-        int[] ans= new int[2];
-        int n = edges.length;
-        int []parent = new int[n+1];
-        int []rank = new int[n+1];
-        for(int i=0;i<n;i++){
+        int []parent = new int[edges.length+1];
+        int []rank = new int[edges.length+1];
+        
+        for(int i=0;i<parent.length;i++){
             parent[i]=i;
-            rank[i]=1;
+            rank[i]=0;
         }
-        for(int i=0;i<n;i++){
-            int left = edges[i][0];
-            int right = edges[i][1];
+        for(int []arr : edges){
+            int left = arr[0];
+            int right = arr[1];
             
-            int lx = find(parent,left);
-            int ly = find(parent,right);
+            int pleft = find(parent,left);
+            int pright = find(parent,right);
             
-            if(lx!=ly){
-                if(rank[lx]<rank[ly]){
-                    parent[lx]=ly;
-                }else if(rank[lx]>rank[ly]){
-                    parent[ly]=lx;
-                }else{
-                    parent[lx]=ly;
-                    rank[ly]++;
-                }
-            }else{
-                ans[0]=left;
-                ans[1]=right;
-                
-                return ans;
+            if(pleft==pright) return arr;
+            else{
+                union(parent,rank,pleft,pright);
             }
         }
-        return ans;
+        return null;
     }
 }
