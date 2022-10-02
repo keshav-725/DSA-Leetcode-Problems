634198bdd8fcdfaf199a2765267917f96430a086
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-03 01:17:36 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (19.84%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -16,9 +16,24 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
+        int left = 0;
+        TreeNode tnode = root.left;
+        while(tnode!=null){
+            tnode=tnode.left;
+            left++;
+        }
+        int right = 0;
+        tnode = root.right;
+        while(tnode!=null){
+            tnode=tnode.right;
+            right++;
+        }
         
-        int sum = countNodes(root.left) + countNodes(root.right) +1;
+        if(left==right){
+            left++;
+            return (1<<left)-1;
+        }
+        return countNodes(root.left)+countNodes(root.right)+1;
         
-        return sum;
     }
 }
