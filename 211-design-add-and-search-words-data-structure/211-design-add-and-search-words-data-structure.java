0e15234e281ff2f9b104e8bd3957cde66e616257
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-06 14:24:14 +0530

    Time: 986 ms (58.41%), Space: 288 MB (20.45%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -1,26 +1,22 @@
 class WordDictionary {
-    public static class Node{
-        private Node[] children = new Node[26];
-        private boolean isEnd = false;
+    class Node{
+        Node []children = new Node[26];
+        boolean isEnd = false;
         
         public boolean contains(char ch){
-            return (children[ch-'a'] != null);
+            return (children[ch-'a']!=null);
         }
-        
         public Node get(char ch){
             return children[ch-'a'];
         }
-        
         public void set(char ch){
             children[ch-'a'] = new Node();
         }
-        
         public boolean getEnd(){
             return isEnd;
         }
-        
-        public void setEnd(boolean isEnd){
-            this.isEnd = isEnd;
+        public void setEnd(){
+            this.isEnd=true;
         }
     }
     Node root;
@@ -28,39 +24,35 @@ class WordDictionary {
         root = new Node();
     }
     
-    public void addWord(String words) {
+    public void addWord(String word) {
         Node curr = root;
-        for(int i=0;i<words.length();i++){
-            char ch = words.charAt(i);
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
             if(curr.contains(ch)==false){
                 curr.set(ch);
             }
             curr = curr.get(ch);
         }
-        curr.setEnd(true);
-    }
-    public boolean search(String word,int idx,Node curr){
-        if(idx == word.length()){
-            return curr.getEnd();
-        }
-        
-        char ch = word.charAt(idx);
-        if(ch!='.'){
-            if(curr.contains(ch)==false) return false;
-           return search(word,idx+1,curr.get(ch));
-        }else{
-            
-        }
-        
-        for(char chn='a';chn<='z';chn++){
-            if(curr.contains(chn)==false) continue;
-            if(search(word,idx+1,curr.get(chn))) return true;
-        }
-        return false;
+        curr.setEnd();
     }
     
     public boolean search(String word) {
-       return search(word,0,root);
+        Node curr = root;
+        return search(word,0,curr);
+    }
+    public boolean search(String word,int idx,Node curr){
+        if(idx==word.length()) return curr.getEnd();
+        char ch = word.charAt(idx);
+        if(ch!='.'){
+            if(curr.contains(ch)==false) return false;
+            return search(word,idx+1,curr.get(ch));
+        }else{
+            for(char chn='a';chn<='z';chn++){
+                if(curr.contains(chn)==false) continue;
+                if(search(word,idx+1,curr.get(chn))) return true;
+            }
+        }
+        return false;
     }
 }
 
