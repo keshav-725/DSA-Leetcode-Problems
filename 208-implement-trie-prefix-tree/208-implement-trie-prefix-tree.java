ecbb88518060a9f274587d8c5538c447f3b9e6eb
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-06 14:01:00 +0530

    Time: 81 ms (48.84%), Space: 67.8 MB (54.08%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -1,26 +1,22 @@
 class Trie {
-    public static class Node{
-        private Node[] children = new Node[26];
-        private boolean isEnd = false;
+    class Node{
+        Node []children = new Node[26];
+        boolean isEnd = false;
         
         public boolean contains(char ch){
-            return (children[ch-'a'] != null);
+            return (children[ch-'a']!=null);
         }
-        
         public Node get(char ch){
             return children[ch-'a'];
         }
-        
         public void set(char ch){
             children[ch-'a'] = new Node();
         }
-        
         public boolean getEnd(){
             return isEnd;
         }
-        
         public void setEnd(boolean isEnd){
-            this.isEnd = isEnd;
+            this.isEnd=isEnd;
         }
     }
     Node root;
@@ -28,10 +24,10 @@ class Trie {
         root = new Node();
     }
     
-    public void insert(String words) {
+    public void insert(String word) {
         Node curr = root;
-        for(int i=0;i<words.length();i++){
-            char ch = words.charAt(i);
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
             if(curr.contains(ch)==false){
                 curr.set(ch);
             }
@@ -40,22 +36,22 @@ class Trie {
         curr.setEnd(true);
     }
     
-    public boolean search(String words) {
+    public boolean search(String word) {
         Node curr = root;
-        for(int i=0;i<words.length();i++){
-            char ch = words.charAt(i);
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
             if(curr.contains(ch)==false){
                 return false;
             }
             curr = curr.get(ch);
         }
-        return curr.getEnd();
+        return curr.getEnd(); 
     }
     
-    public boolean startsWith(String words) {
+    public boolean startsWith(String word) {
         Node curr = root;
-        for(int i=0;i<words.length();i++){
-            char ch = words.charAt(i);
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
             if(curr.contains(ch)==false){
                 return false;
             }
