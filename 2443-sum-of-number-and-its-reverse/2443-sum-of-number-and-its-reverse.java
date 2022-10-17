eafe1acad62fab0a0ca50efdbae5e87e5499c215
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-17 23:53:01 +0530

    Time: 1181 ms (100.00%), Space: 143.9 MB (100.00%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.java
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.java
@@ -1,21 +1,10 @@
 class Solution {
     public boolean sumOfNumberAndReverse(int num) {
         for(int val=num/2;val<=num;val++){
-            int rval = num-val;
-            if(check(val,rval)) return true;
+            StringBuilder sb = new StringBuilder(val+"");
+            int trev = Integer.parseInt(sb.reverse().toString());
+            if(val+trev == num) return true;
         }
-        return false;
-    }
-    public boolean check(int n1,int n2){
-        StringBuilder sb1 = new StringBuilder(n1+"");
-        StringBuilder sb2 = new StringBuilder(n2+"");
-        while(sb1.length()>sb2.length()){
-            sb2.insert(0,'0');
-        }
-        while(sb2.length()>sb1.length()){
-            sb1.insert(0,'0');
-        }
-        if(sb1.reverse().toString().equals(sb2.toString())) return true;
         return false;
     }
 }
