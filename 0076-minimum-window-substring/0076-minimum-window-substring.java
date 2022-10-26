e7bf4b8d193a52616a55b101739eb5b1811fb0fe
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-26 16:14:39 +0530

    Time: 168 ms (11.14%), Space: 43.2 MB (83.14%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,48 +3,39 @@ class Solution {
         HashMap<Character,Integer> map1 = new HashMap<>();
         HashMap<Character,Integer> map2 = new HashMap<>();
         
-        if(t.length()>s.length()) return "";
-        
         for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+        }
+        int j = 0,ans=Integer.MAX_VALUE;
+        String res = "";
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
             map1.put(ch,map1.getOrDefault(ch,0)+1);
-        }
-        int count = t.length();
-        int potcount = 0;
-        for(int i=0;i<t.length();i++){
-            char ch = s.charAt(i);
-            if(map1.containsKey(ch) && map1.get(ch)>map2.getOrDefault(ch,0)){
-                potcount++;
-            }
-            map2.put(ch,map2.getOrDefault(ch,0)+1);
-        }
-        int i = t.length();
-        int j = 0;
-        String ans = "";
-        int minlen = Integer.MAX_VALUE;
-        if(potcount==count){
-            return s.substring(j,i);
-        }
-        while(i<s.length()){
-            char ch = s.charAt(i);
-            if(map1.containsKey(ch) && map1.get(ch)>map2.getOrDefault(ch,0)){
-                potcount++;
-            }
-            map2.put(ch,map2.getOrDefault(ch,0)+1);
-            while(potcount==count){
-                if(minlen>i-j){
-                    ans = s.substring(j,i+1);
-                    minlen = i-j+1;
+            while(isExist(map1,map2)){
+                // System.out.println("i-> "+i);
+                // System.out.println(map1);
+                if(ans>(i-j+1)){
+                    ans = i-j+1;
+                    res = s.substring(j,i+1);
                 }
-                char prevch = s.charAt(j);
-                if(map1.containsKey(prevch) && map2.get(prevch)<=map1.get(prevch)){
-                    potcount--;
-                }
-                map2.put(prevch,map2.get(prevch)-1);
+                char jch = s.charAt(j);
                 j++;
+                map1.put(jch,map1.get(jch)-1);
+                if(map1.get(jch)==0) map1.remove(jch);
             }
-            i++;
         }
-        return ans;
+        return res;
+    }
+    public boolean isExist(HashMap<Character,Integer> map1,HashMap<Character,Integer> map2){
+        
+        for(char ch : map2.keySet()){
+            if(map1.containsKey(ch) && map1.get(ch)>=map2.get(ch)){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
 }
