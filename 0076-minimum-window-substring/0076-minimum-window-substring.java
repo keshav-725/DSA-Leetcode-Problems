230794db9b606e3aac40fa39532c76c57873934e
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-26 16:31:43 +0530

    Time: 33 ms (31.40%), Space: 54.1 MB (13.85%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -7,35 +7,29 @@ class Solution {
             char ch = t.charAt(i);
             map2.put(ch,map2.getOrDefault(ch,0)+1);
         }
-        int j = 0,ans=Integer.MAX_VALUE;
+        
+        int count = t.length();
+        int pcount = 0,ans=Integer.MAX_VALUE,j=0;
         String res = "";
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
+
+            if(map2.getOrDefault(ch,0)>map1.getOrDefault(ch,0)) pcount++;
+            
             map1.put(ch,map1.getOrDefault(ch,0)+1);
-            while(isExist(map1,map2)){
-                // System.out.println("i-> "+i);
-                // System.out.println(map1);
+            
+            while(pcount == count){
                 if(ans>(i-j+1)){
                     ans = i-j+1;
                     res = s.substring(j,i+1);
                 }
                 char jch = s.charAt(j);
-                j++;
+                if(map2.getOrDefault(jch,0)>=map1.getOrDefault(jch,0)) pcount--;
                 map1.put(jch,map1.get(jch)-1);
-                if(map1.get(jch)==0) map1.remove(jch);
+                j++;
             }
+            
         }
         return res;
-    }
-    public boolean isExist(HashMap<Character,Integer> map1,HashMap<Character,Integer> map2){
-        
-        for(char ch : map2.keySet()){
-            if(map1.containsKey(ch) && map1.get(ch)>=map2.get(ch)){
-                continue;
-            }else{
-                return false;
-            }
-        }
-        return true;
     }
 }
