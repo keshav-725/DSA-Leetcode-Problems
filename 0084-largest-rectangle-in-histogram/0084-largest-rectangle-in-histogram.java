584caaada7406ef183c7c880bb0fd3ab55bc71df
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-10-30 12:25:28 +0530

    Time: 181 ms (60.06%), Space: 80 MB (74.92%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,44 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int []left = new int[n];
-        int []right = new int[n];
         
         Stack<Integer> st = new Stack<>();
-        
-        for(int i=0;i<heights.length;i++){
-            int num = heights[i];
-            
-            while(st.size()>0 && heights[st.peek()]>num){
-                st.pop();
-            }
-            if(st.size()==0){
-                left[i] = -1;
-            }else{
-                left[i] = st.peek();
-            }
-            st.push(i);
-        }
-        
-        st = new Stack<>();
-        
-        for(int i=heights.length-1;i>=0;i--){
-            int num = heights[i];
-            
-            while(st.size()>0 && heights[st.peek()]>=num){
-                st.pop();
-            }
-            if(st.size()==0){
-                right[i] = heights.length;
-            }else{
-                right[i] = st.peek();
-            }
-            st.push(i);
-        }
         int max = 0;
-        for(int i=0;i<heights.length;i++){
-            max = Math.max((right[i]-left[i]-1)*heights[i],max);
-            // System.out.println("left-> "+left[i]+" right-> "+right[i]);
+        
+        for(int i=0;i<=heights.length;i++){
+            
+            while(st.size()>0 && (i==n || heights[st.peek()]>=heights[i])){
+                int temp = heights[st.pop()];
+                int width;
+                if(st.size()==0){
+                    width = i;
+                }else{
+                    width = i-st.peek()-1;
+                }
+                
+                max = Math.max(width*temp,max);
+            }
+            st.push(i);
         }
         return max;
     }
