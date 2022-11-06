41698a8bd5eafe47b0cc1de4e5534bb33ef04e12
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-11-07 02:09:23 +0530

    Time: 22 ms (9.58%), Space: 42 MB (69.06%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,43 +1,99 @@
 class Twitter {
-    class Pair{
-        int userId;
-        int tweetId;
-        Pair(int userId,int tweetId){
-            this.userId = userId;
-            this.tweetId = tweetId;
+    private static int timestamp = 0;
+    Map<Integer,User> map;
+    public class Tweet{
+        int id;
+        int time;
+        Tweet next;
+        
+        public Tweet(int id){
+            this.id = id;
+            time = ++timestamp;
+            next = null;
         }
     }
-    HashMap<Integer,HashSet<Integer>> followers;
-    ArrayList<Pair> tweets;
+    public class User{
+        int userId;
+        Set<Integer> followed;
+        Tweet tweet_head;
+        
+        public User(int id){
+            this.userId = id;
+            tweet_head=null;
+            followed = new HashSet<>();
+            follow(id);
+        }
+        public void follow(int followee){
+            followed.add(followee);
+        }
+        public void unfollow(int followee){
+            followed.remove(followee);
+        }
+        public void post(int tweetId){
+            Tweet tweet = new Tweet(tweetId);
+            tweet.next = tweet_head;
+            tweet_head = tweet;
+        }
+    }
     public Twitter() {
-        followers = new HashMap<>();
-        tweets = new ArrayList<>();
+        map = new HashMap<>();
     }
     
     public void postTweet(int userId, int tweetId) {
-        tweets.add(new Pair(userId,tweetId));
+        if(!map.containsKey(userId)){
+            User u = new User(userId);
+            map.put(userId,u);
+        }
+        map.get(userId).post(tweetId);
     }
     
     public List<Integer> getNewsFeed(int userId) {
-        List<Integer> newsFeed = new ArrayList<>();
-        int count = 10;
-        for(int i=tweets.size()-1;i>=0 && count>0;i--){
-            Pair p = tweets.get(i);
-            if((followers.containsKey(userId) && followers.get(userId).contains(p.userId)) || userId==p.userId){
-                newsFeed.add(p.tweetId);
-                count--;
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        if(!map.containsKey(userId)) return ans;
+        
+        Set<Integer> users = map.get(userId).followed;
+        PriorityQueue<Tweet> pq = new PriorityQueue<>(users.size(),(a,b)->(b.time-a.time));
+        
+        for(int user : users){
+            Tweet t = map.get(user).tweet_head;
+            
+            if(t!=null){
+                pq.add(t);
             }
         }
-        return newsFeed;
+        int n = 0;
+        while(pq.size()>0 && n<10){
+            Tweet t = pq.poll();
+            
+            ans.add(t.id);
+            
+            if(t.next != null){
+                pq.add(t.next);
+            }
+            n++;
+        }
+        return ans;
     }
     
     public void follow(int followerId, int followeeId) {
-        followers.putIfAbsent(followerId,new HashSet<>());
-        followers.get(followerId).add(followeeId);
+        if(!map.containsKey(followerId)){
+            User u = new User(followerId);
+            map.put(followerId,u);
+        }
+        if(!map.containsKey(followeeId)){
+            User u = new User(followeeId);
+            map.put(followeeId,u);
+        }
+        map.get(followerId).follow(followeeId);
     }
     
     public void unfollow(int followerId, int followeeId) {
-        if(followers.containsKey(followerId)) followers.get(followerId).remove(followeeId);
+        if(!map.containsKey(followeeId) || followerId==followeeId){
+            return;
+        }
+        map.get(followerId).unfollow(followeeId);
     }
 }
 
