b33fc5da85d2180e63528e415091b359399d1160
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-11-12 12:11:04 +0530

    Time: 50 ms (26.25%), Space: 60.2 MB (14.25%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,31 +1,28 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq;
+        
         int []dp = new int[n+1];
-        dp[1]=1;
-        
+        dp[1] = 1;
         int tidx=1,thidx=1,fidx=1;
-        
         for(int i=2;i<=n;i++){
-            pq=new PriorityQueue<>();
+            pq = new PriorityQueue<>();
             pq.add(2*dp[tidx]);
             pq.add(3*dp[thidx]);
             pq.add(5*dp[fidx]);
             
-            int rem = pq.remove();
-            dp[i] = rem;
+            dp[i] = pq.poll();
             
-            if(rem==2*dp[tidx]){
+            if(dp[i]==2*dp[tidx]){
                 tidx++;
             }
-            if(rem==3*dp[thidx]){
+            if(dp[i]==3*dp[thidx]){
                 thidx++;
             }
-            if(rem==5*dp[fidx]){
+            if(dp[i]==5*dp[fidx]){
                 fidx++;
             }
         }
-        
         return dp[n];
     }
 }
