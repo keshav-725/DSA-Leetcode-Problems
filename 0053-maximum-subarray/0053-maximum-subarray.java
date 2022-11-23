52be901448a776be6473c6ac815a0412e651379b
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2022-11-23 22:16:51 +0530

    Time: 1 ms (100.00%), Space: 51.5 MB (94.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,19 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        Integer [][]dp = new Integer[2][nums.length];
-        return helper(nums,0,false,dp);
-        
-    }
-    public int helper(int []nums,int idx,boolean check,Integer [][]dp){
-        if(idx==nums.length){
-            if(check) return 0;
-            return Integer.MIN_VALUE;
+        int ans = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+            ans = Math.max(sum,ans);
+            if(sum<0){
+                sum = 0;
+            }
         }
-        if(check){
-            if(dp[1][idx]!=null) return dp[1][idx];
-            return dp[1][idx]=Math.max(0,nums[idx]+helper(nums,idx+1,true,dp));
-        }
-        if(dp[0][idx]!=null) return dp[0][idx];
-        return dp[0][idx]=Math.max(helper(nums,idx+1,false,dp),nums[idx]+helper(nums,idx+1,true,dp));
+        return ans;
     }
 }
