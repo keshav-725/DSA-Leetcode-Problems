8e6e431fd991702ef89ec0d585c7c44979c080dc
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2023-03-01 00:25:16 +0530

    Time: 4 ms (86.97%), Space: 42.2 MB (81.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,15 +1,30 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length(),n = word2.length();
-        int dp[][] = new int[m+1][n+1];
-        for(int i=0; i<=m; i++){
-            for(int j=0; j<=n; j++){
-                if(i == 0) dp[i][j] = j;
-                else if(j == 0) dp[i][j] = i;
-                else if(word1.charAt(i-1) == word2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
-            }
+        int n=word1.length(),m=word2.length();
+        int [][]dp = new int[n+1][m+1];
+        for(int []arr : dp){
+            Arrays.fill(arr,-1);
         }
-        return dp[m][n];
+        return helper(word1,word2,n,m,dp);
+    }
+    public int helper(String word1,String word2,int n,int m,int [][]dp){
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(n==0){
+            dp[n][m]=m;
+            return m;
+        }
+        if(m==0){
+            dp[n][m]=n;
+            return n;
+        }
+        if(word1.charAt(n-1)==word2.charAt(m-1)){
+            return dp[n][m]=helper(word1,word2,n-1,m-1,dp);
+        }else{
+            int insert = helper(word1,word2,n,m-1,dp);
+            int delete = helper(word1,word2,n-1,m,dp);
+            int replace = helper(word1,word2,n-1,m-1,dp);
+            int min = Math.min(insert,Math.min(delete,replace));
+            return dp[n][m]=1+min;
+        }
     }
 }
