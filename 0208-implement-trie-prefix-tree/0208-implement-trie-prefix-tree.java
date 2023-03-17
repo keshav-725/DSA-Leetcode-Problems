febe1e4105fd44730c1fc1dca7dd85d1b8a16ae4
keshav kumar <70107803+keshav-725@users.noreply.github.com>
2023-03-17 18:46:35 +0530

    Time: 39 ms (57.64%), Space: 51.6 MB (66.35%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,70 @@
+class Trie {
+    class Node{
+        Node []children = new Node[26];
+        boolean isEnd = false;
+        
+        public boolean contains(char ch){
+            return (children[ch-'a']!=null);
+        }
+        public Node get(char ch){
+            return children[ch-'a'];
+        }
+        public void set(char ch){
+            children[ch-'a'] = new Node();
+        }
+        public boolean getEnd(){
+            return isEnd;
+        }
+        public void setEnd(boolean isEnd){
+            this.isEnd=isEnd;
+        }
+    }
+    Node root;
+    public Trie() {
+        root = new Node();
+    }
+    
+    public void insert(String word) {
+        Node curr = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(curr.contains(ch)==false){
+                curr.set(ch);
+            }
+            curr = curr.get(ch);
+        }
+        curr.setEnd(true);
+    }
+    
+    public boolean search(String word) {
+        Node curr = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(curr.contains(ch)==false){
+                return false;
+            }
+            curr = curr.get(ch);
+        }
+        return curr.getEnd(); 
+    }
+    
+    public boolean startsWith(String word) {
+        Node curr = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(curr.contains(ch)==false){
+                return false;
+            }
+            curr = curr.get(ch);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
